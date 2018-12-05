69ca2e0e13d772d500b6fa6ec53dafd6c61cebc4
stefinfection <sjeangeo@gmail.com>
2018-12-05 09:47:27 -0700

    Implemented multi-stream algorithm

--- src/main.cc
+++ src/main.cc
@@ -51,15 +51,99 @@ int main(int argc, const char *argv[]) {
         recordArr.push_back(records);
     }
 
-    // ***COMBINE HEADER INFO AND OUTPUT
-    // TODO: need all phase info here to coordinate with chips, for now just outputting first header
-    htsOutHandle << headers[0];
+
+    // ***PRIME LOOP
+    // Load first record from each file into our working array
+    std::vector<bcfRecord*> currRecs(numFiles);                 // Vector of individual lines - one per file and in relative order to files/headers
+    for (int i = 0; i < numFiles; i++) {
+        currRecs.push_back(&(recordArr[i][0]));
+    }
+    bool advanceMin = false;        // True if we've seen all lines at or below the current coordMin
+    int coordMin = INFINITY;        // The minimum coordinate in currRecs per iteration
+    bcfHeader* outHdr = nullptr;       // The combined header for the output line
+    bcfRecord* outLine = nullptr;      // The next line to be written to the output stream
+    bool outOfLines = false;        // True if we have any remaining lines to be processed in any files
 
 
+    // ***PROCESS LINES
+    // Find minimum coordinate
+    while (!outOfLines) {
+        for (int i = 0; i < numFiles; i++) {
+            int currCoord = (*currRecs[i])->pos + 1;
+            if (currCoord < coordMin) {
+                coordMin = currCoord;
+            }
+        }
+        // Retrieve lines with the minimum coordinate
+        while(!advanceMin) {
+            bool noLines = true;
+            bool noLineAtMin = true;
+            for (int i = 0; i < numFiles; i++) {
+                int currCoord = (*currRecs[i])->pos + 1;
+
+                // Found coordinate match
+                if (currCoord == coordMin) {
+                    noLineAtMin = false;
+                    // If first line at current coordinate
+                    if (outLine == NULL) {
+                        outHdr = &headers[i];
+                        outLine = currRecs[i];
+                        // TODO: removeInfo(outHdr, outLine); - remove all INFO fields besides genotypes for simplicity & browser memory issues? [optional]
+                    // Combine lines
+                    } else {
+                        // Get genotypes out of outLine
+                        int status, *out_gt_arr = nullptr, out_ngt_arr = 0;
+                        status = bcf_get_genotypes((*outHdr).get(), (*outLine).get(), &out_gt_arr, &out_ngt_arr);
+                        if (status <= 0) {
+                            logger(LOGLV_WARN) << "skipping record at " << bcf_hdr_id2name((*outHdr).get(), (*outLine)->rid)
+                                               << ":" << (*outLine)->pos + 1 << " - could not obtain genotype" << std::endl;
+                            continue;
+                        }
+                        std::unique_ptr<int> uniq_out_gt_arr(out_gt_arr);
+
+                        // Get genotypes out of current line
+                        int *curr_gt_arr = nullptr, curr_ngt_arr = 0;
+                        status = bcf_get_genotypes(headers[i].get(), (*currRecs[i]).get(), &curr_gt_arr, &curr_ngt_arr);
+                        if (status <= 0) {
+                            logger(LOGLV_WARN) << "skipping record at " << bcf_hdr_id2name(headers[i].get(), (*currRecs[i])->rid)
+                                               << ":" << (*currRecs[i])->pos + 1 << " - could not obtain genotype" << std::endl;
+                            continue;
+                        }
+                        std::unique_ptr<int> uniq_curr_gt_arr(curr_gt_arr);
+
+                        // Combine genotypes and header info
+                        // TODO
+                    }
+                    // Put next line in spot we just pulled out
+                    currCoord = getNextLine(currRecs[i]);
+                    // TODO: noLines &= endOfFile
+
+                // Sanity check for testing
+                } else if (currCoord < coordMin) {
+                    logger(LOGLV_WARN) << "ERROR: minimum is not minimum";
+                }
+            }
+            advanceMin = noLineAtMin;
+            outOfLines = noLines;
+        }
+
+        // Output line - guaranteed to have all files processed at min here
+        htsOutHandle << bcfHdrRecPair(*outHdr, *outLine);
+
+        // Reset loop parameters
+        outLine = nullptr;
+        outHdr = nullptr;
+        coordMin = INFINITY;
+    }
+
+
+
+
+    // <editor-fold desc="Combine All Records">
     // ***COMBINE ALL RECORD ARRAYS INTO SINGLE ARRAY
-    std::vector<int> lineIndex(numFiles, 0);            // Array of line index for which record looking at in each file
-    std::vector<bcfRecord*> currRecs(numFiles);         // Array of the current record being compared for each file
-    std::vector<bcfRecord> combinedRecs;                // Giant array of ordered, combined lines
+    /*std::vector<int> lineIndex(numFiles, 0);                // Array of line index for which record looking at in each file
+    std::vector<bcfRecord *> currRecs(numFiles);            // Array of the current record being compared for each file
+    std::vector<bcfRecord> combinedRecs;                    // Giant array of ordered, combined lines
 
     // Initialize index array and put first line of each file in current record array
     for (int i = 0; i < numFiles; i++) {
@@ -74,15 +158,16 @@ int main(int argc, const char *argv[]) {
     int lowCoord = INFINITY;
     while (!allFilesFinished) {
         for (int i = 0; i < numFiles; i++) {
-            if (!fileFinished[i] && (*currRecs[i])->pos+1 <= lowCoord) {
-                lowCoord = (*currRecs[i])->pos+1;
+            if (!fileFinished[i] && (*currRecs[i])->pos + 1 <= lowCoord) {
+                lowCoord = (*currRecs[i])->pos + 1;
                 lowIndex = i;
             }
         }
 
         // Add lowest record & fill in spot
-        combinedRecs.push_back(std::move(*currRecs[lowIndex]));             // Transfer control of pointer to combined array
-        lineIndex[lowIndex] = lineIndex[lowIndex]++;                        // Advance current line in file we just pulled from
+        combinedRecs.push_back(
+                std::move(*currRecs[lowIndex]));                    // Transfer control of pointer to combined array
+        lineIndex[lowIndex] = lineIndex[lowIndex]++;                // Advance current line in file we just pulled from
         if (lineIndex[lowIndex] < recordArr[lowIndex].size()) {
             currRecs[lowIndex] = &recordArr[lowIndex][lineIndex[lowIndex]]; // Fill in next line from file
         } else {
@@ -97,212 +182,69 @@ int main(int argc, const char *argv[]) {
         for (int i = 0; i < numFiles; i++) {
             allFilesFinished &= fileFinished[i];
         }
-    }
+    }*/
+    //</editor-fold>
 
 
-    // ***REMOVE DUPLICATES FROM COMBINED, ORDERED ARRAY
+    // ***COMBINE ALL CALLS FOR A SINGLE SITE
     int currLine = 0;
     int nextLine = 1;
+    bool matchFlag = false;
+    // declare start line array of enrichment counts here
+
     while (nextLine < combinedRecs.size()) {
-        if (combinedRecs[currLine]->pos+1 == combinedRecs[nextLine]->pos+1) {
-            // TODO: combine all lines at the same position into a single, multi-allelic line
-            // This will require calling unpack on the struct to get allele info
-            // Can set max_unpack to pull in just allele at first, and then re-unpack if combining required?
-            if (combinedRecs[currLine]->alt == combinedRecs[nextLine]->alt) {
+        if (combinedRecs[currLine]->pos + 1 == combinedRecs[nextLine]->pos + 1) {
+            matchFlag = true;
+
+            // Pull out gt array for start line like below - store in outside prop
+
+
+
+            // Pull out gt array for next line
+            int status, *next_gt_arr = nullptr, next_ngt_arr = 0;
+
+            // TODO: have to have header lookup here
+            status = bcf_get_genotypes(header.get(), combinedRecs[nextLine].get(), &next_gt_arr, &next_ngt_arr);
+            if (status <= 0) {
+                logger(LOGLV_WARN) << "skipping record at " << bcf_hdr_id2name(header.get(), rec->rid)
+                                   << ":" << rec->pos + 1 << " - could not obtain genotype" << std::endl;
+                continue;
             }
-            else {
-                htsOutHandle << bcfHdrRecPair(headers[0], combinedRecs[currLine]);  // TODO: use combined header here
+            std::unique_ptr<int> uniq_gt_arr(next_gt_arr);
+
+
+            // for each alt in next_gt_arr
+                // if (start_gt_arr.contains(alt)
+                    // find alt index of this alt in start
+                    // combine counts from next at pulled index - helper fxn
+                // else
+                    // append alt to start_gt_arr
+                    // combine enrichment counts for alt into start enrichment counts - helper fxn
+
+        } else {
+            if (matchFlag) {
+                // Output combined line at start             htsOutHandle << bcfHdrRecPair(headers[0], combinedRecs[currLine]);  // TODO: use combined header here
+                matchFlag = false;
                 currLine = nextLine;
+            } else {
+                // Just output line at start? TODO: flesh this out
             }
-            nextLine++;
         }
+        nextLine++;
     }
 }
 
 
-
-
-
-
-
-
-
-// *** OTHER APPROACHES / MUSINGS...
-
-
-//    std::vector<int> currMap(numFiles, 0);              // Lookup for recordArr - static position matches coord array, dynamic value matches record position
-//    std::vector<int> nextMap(numFiles, 0);
-//
-//    std::vector<int> currCoords(numFiles, 0);           // Coordinates for current position
-//    std::vector<int> nextCoords(numFiles, 0);           // Coordinates for next position
-//
-//    std::vector<std::string> currAlts(numFiles, "");    // Variants at current position
-//    std::vector<std::string> nextAlts(numFiles, "");    // Variants at next position
-//
-//    std::vector<std::vector<int>> currCounts;           // Counts for current position
-//    std::vector<std::vector<int>> nextCounts;           // Counts for next position
-
-
-// ***HELPER FXNS
-//htsIterator advanceAndFill(htsIterator record, std::vector<int> &coords, std::vector<std::string> &alts, std::vector<int*> &counts, int arrIndex, int advanceNum);
-
-//    void sortAndCombine(std::vector<htsIterator> &currRecords, std::vector<htsIterator> &nextRecords, std::vector<int> &currCoords, std::vector<int> &nextCoords,
-//            std::vector<std::string> &currAlts, std::vector<std::string> &nextAlts, std::vector<int*> &currCounts, std::vector<int*> &nextCounts);
-
-
-
-//    // ***INITIALIZE ARRAYS
-//    for (int i = 0; i < numFiles; i++) {
-//
-//    }
-//
-//
-//        // TODO: might make below into a callable loop w/ line # to retrieve records by (instead of hard coded 0 and 1)
-//        // Get current fields and put in respective arrays
-//        currCoords[i] = records[0]->pos + 1;
-//
-//        // Get alt strings and put in array
-//        // TODO: have to call vcf_unpack here
-//
-//        // Get counts and put in array
-//        int numAlts = (records[0]->n_allele) - 1;
-//        int countArrLength = (numAlts * 2) + 2;
-//        std::vector<int> counts((countArrLength * 2), 0);
-//        int status = bcf_get_info_int32(header.get(), &(records[0]), "ENRICH_COUNTS", &counts[0], (countArrLength * 2));     // TODO: why doesn't this work?
-//        if (status < 0) {
-//            logger(LOGLV_WARN) << "Could not obtain enrichment counts at " << bcf_hdr_id2name(header.get(), records[0]->rid)
-//                << ":"
-//                << records[0]->pos + 1 << std::endl;
-//        }
-//        currCounts[0] = counts;
-//
-//        // Advance iterator and get next fields
-//        nextCoords[i] = records[1]->pos + 1;
-//        // TODO: get alt string(s) and put in array - copy above when complete
-//        // TODO: get counts from INFO field and put in array - copy above when complete
-//
-//
-//    // ***MAIN LOOP
-//    int finishCount = 0;
-//    while (finishCount < numFiles) {
-//        for (int i = 0; i < numFiles; i++) {
-//
-//            // Stable sort the arrays and combine any duplicates
-//            sortAndCombine(currRecords, nextRecords, currCoords, nextCoords, currAlts, nextAlts, currCounts,
-//                           nextCounts);
-//
-//            // Get first record and write out
-//            auto rec = std::move(currRecords[i]);
-//            htsOutHandle << rec;                        // **Why doesn't this work?
-//
-//            // Advance to the next-next record and fill the coordinate, alt, and count arrays
-//            rec = std::move(advanceAndFill(std::move(rec), currCoords, currAlts, currCounts, i, 2));
-//
-//            // Find next smallest coordinate
-//            if (nextCoords[i] < currCoords[i + 1]) {
-//                rec = std::move(nextRecords[i]);
-//                htsOutHandle << rec;
-//                rec = std::move(advanceAndFill(std::move(rec), nextCoords, nextAlts, nextCounts, i, 2));
-//            } else if (currCoords[i + 1] < nextCoords[i]) {
-//                rec = std::move(currRecords[i + 1]);
-//                htsOutHandle << rec;
-//                rec = std::move(advanceAndFill(std::move(rec), currCoords, currAlts, currCounts, i + 1, 2));
-//            }
-//            // If we get here, we have a tie for INF, meaning we're at the end of both files
-//            else {
-//                finishCount += 2;
-//            }
-//        }
-//    }
-//    return 0;
-//}
-//
-///* Retrieves control of unique pointer to which lineNum corresponds. Obtains relative information and overwrites current info in given arrays as appropriate at the given index.
-// * Returns control of unique record pointer to array.
-// *
-// * NOTE: ASSUMES ONLY VALID INDICES ARE PROVIDED
-// */
-//void advanceAndFill(int index, int lineNum, std::vector<std::vector<bcfRecord>> recordArr, std::vector<bcfHeader> headers,
-//        std::vector<int> &map, std::vector<int> &coords, std::vector<std::string> &alts, std::vector<int*> &counts) {
-//
-//    // Get record that map points to
-//    int fileIndex = map[index];
-//    auto rec = std::move((recordArr[fileIndex])[lineNum]);
-//
-//    // Get header that map points to
-//    auto hdr = std::move(headers[fileIndex]);
-//
-//
-//}
-//
-///* Takes in current and next arrays. Stable sorts all arrays in same relative order. Then searches for duplicates and combines as necessary. */
-//void sortAndCombine() {
-//    // TODO: implement - this will be the pain in the butt to get correct
-//}
-//
-///* Takes in record, coordinate, alternate, and count arrays, along with indices to swap. Performs swap. */
-//void swap() {
-//    // TODO: implement
-//}
-//
-///* Takes in iterator to single file, copies all lines as smart pointers to vector, returns vector and control of smart pointers. */
-//std::vector<bcfRecord> bcf2vec(htsReader<bcfRecord>::iterator begin, htsReader<bcfRecord>::iterator end) {
-//    std::vector<bcfRecord> records;
-//    std::for_each(std::move(begin), std::move(end), [&records](const auto &rec) {
-//       bcfRecord dupRec{bcf_dup(rec.get())};
-//       records.push_back(std::move(dupRec));        // Record pointer only exists in array - pass reference around
-//    });
-//    return std::move(records);
-//}
-
-
-
-
-
-
-//    MAY NEED TO USE NON-WRAPPED VERSION
-//    std::vector<::htsFile*> files(numFiles, nullptr);
-//    std::vector<bcf_hdr_t*> headers(numFiles, nullptr);
-//    std::vector<bcf1_t*> lines(numFiles, nullptr);
-//
-//    for (int i = 0; i < numFiles; i++) {
-//        files[i] = HTSLIB_VCF_H::vcf_open(argv[i+1], "r");
-//        headers[i] = HTSLIB_VCF_H::bcf_hdr_read(files[i]);
-//
-//        // Read next line
-//        HTSLIB_VCF_H::vcf_read(files[i], headers[i], lines[i]);
-//
-//        // TEST: Output line
-//        htsOutHandle << lines[i];
-//    }
-
-//  TODO: can't seem to get Yi's lib to work - ask him if I'm reinventing the wheel
-//    std::vector<YiCppLib::HTSLibpp::htsFile> files;
-//    std::vector<YiCppLib::HTSLibpp::htsHeader<bcfHeader>> headers;
-//    std::vector<YiCppLib::HTSLibpp::htsReader<bcfRecord>> lines;
-
-//    for (int i = 0; i < numFiles; i++) {
-//        auto currFile = YiCppLib::HTSLibpp::htsOpen(argv[i+1], "r");
-//        //auto currHdr = YiCppLib::HTSLibpp::htsHeader<bcfHeader>::read(currFile);
-//        auto currHdr = YiCppLib::HTSLibpp::htsHeader<bcfHeader>::read(currFile);
-//        headers[i] = std::move(currHdr);
-//    }
-
-//    // Pipe any single header out
-//    auto header = htsHeader<bcfHeader>::read(files[0]);
-//    if (header.get() == nullptr) {
-//        std::cerr << "Unable to read header from first file. Please try again." << std::endl;
-//        exit(1);
-//    }
-//    htsOutHandle << header;
-
-// TODO: implement multi-loop iterations
-//    std::vector<bcf1_t*> linePtrs(numFiles, nullptr);
-//    for (int i = 0; i < numFiles; i++) {
-//
-//        // Get pointers to first line for each file
-//        header = htsHeader<bcfHeader>::read(htsInHandles[i]);
-//        auto rec = htsReader<bcfRecord>::read(htsInHandles[i], header);
-//
-//
-//    }
+// SUBLOOP
+// foreach line in currLines
+    // if (line.pos == globalMin)
+        // if (outputLine empty) outputLine = line & clear out INFO fields other than gt;
+        // else combine genotypes from line into genotypes from outputLine;
+        // advance line
+// get nextMin of lines
+    // if nextMin > currMin
+        // calculate enrichment for line and append to INFO field
+        // write outputLine
+        // outputLine - NULL
+    // else
+        // goto foreach
